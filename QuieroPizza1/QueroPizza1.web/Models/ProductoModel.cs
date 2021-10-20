2ff08280f3ee208420909b2c5546fb8a3e8fd470
Brayan Abdubal <Brayan Abdubal@DESKTOP-1V2SH02>
2021-10-20 11:31:02 -0600

    Agregamos Lista de producto

---
+++ QuieroPizza1/QueroPizza1.web/Models/ProductoModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QueroPizza1.web.Models
+{
+    public class ProductoModel
+    {
+        public int id { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
